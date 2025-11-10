1b92ecf073ee4253cad6f0b0479492ee3c3be15b
Thembelani Ngema <thembelaningema20@gmail.com>
2025-11-10 20:56:31 +0200

    JSON -> <- POJO using Jackson Data Binding. Created an api that returns list of books

--- src/main/java/com/thembelani/books/controller/BookController.java
+++ src/main/java/com/thembelani/books/controller/BookController.java
@@ -3,11 +3,33 @@ package com.thembelani.books.controller;
 //First part of the application that will consume the API endpoint,
 // so we know what is sent in and will return data to the client
 
+import com.thembelani.books.entity.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class BookController {
+
+    private final List<Book> books = new ArrayList<>();
+
+    public BookController() {
+        initializeBooks();
+    }
+
+    private void initializeBooks() {
+
+        books.addAll(List.of(
+                new Book("Title One", "Author One", "Science"),
+                new Book("Title Two", "Author One", "Science"),
+                new Book("Title Three", "Author One", "History"),
+                new Book("Title Four", "Author One", "Math"),
+                new Book("Title Five", "Author One", "Math"),
+                new Book("Title Six", "Author One", "Math")
+        ));
+    }
 
     @GetMapping  //no path specified, so this means at the root path, the below method will be called
     public String firstAPI() {
@@ -15,7 +37,13 @@ public class BookController {
     }
 
     @GetMapping("/api")
-    public String firstAPIPath(){
+    public String firstAPIPath() {
         return "Thembelani Ngema!";
     }
+
+    @GetMapping("/api/books")
+    public List<Book> getBooks() {
+        return books;
+    }
+
 }
