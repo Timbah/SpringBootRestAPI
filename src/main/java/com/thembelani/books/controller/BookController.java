6226a731d1c480348ff224cdee81291b6e6c4f9f
Thembelani Ngema <thembelaningema20@gmail.com>
2025-11-11 21:25:24 +0200

    Added post endpoint example with streams

--- src/main/java/com/thembelani/books/controller/BookController.java
+++ src/main/java/com/thembelani/books/controller/BookController.java
@@ -67,11 +67,11 @@ public class BookController {
     @PostMapping("/api/books")
     public void createBook(@RequestBody Book newBook) {
 
-        for (Book book : books) {
-            if (book.getTitle().equalsIgnoreCase(newBook.getTitle())) {
-                return;
-            }
+        boolean isNewBook = books.stream()
+                .noneMatch(book -> book.getTitle().equalsIgnoreCase(newBook.getTitle()));
+
+        if(isNewBook){
+            books.add(newBook);
         }
-        books.add(newBook);
     }
 }
