b7cf03ee0f33c4d668f5238828886b025b70c94d
Thembelani Ngema <thembelaningema20@gmail.com>
2025-11-16 22:37:06 +0200

    Global exception handling

--- src/main/java/com/thembelani/books/controller/BookController.java
+++ src/main/java/com/thembelani/books/controller/BookController.java
@@ -134,4 +134,15 @@ public class BookController {
         );
         return new ResponseEntity<>(bookErrorResponse, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler  //Default exception handling for any other EXCEPTION not being book not found exists
+    public ResponseEntity<BookErrorResponse> handleException(Exception exc) {
+
+        BookErrorResponse bookErrorResponse = new BookErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                "Invalid Request",
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(bookErrorResponse, HttpStatus.BAD_REQUEST);
+    }
 }
